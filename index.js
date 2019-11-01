f73969688ad72a555fc4eaae72ec7bbade317e1a
Jack <29169102+Jack5079@users.noreply.github.com>
2019-10-31 22:53:52 -0400

    fix cheat

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const count = document.querySelector('#count')
 count.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     const a = document.querySelector('a')
-    a.href = `javascript: while (beagleCount < ${count.value}) document.querySelector('#beagle').onclick(); void 0;`
+    a.href = `javascript: while (beagleCount < ${count.value}) document.querySelector('#beagle').click(); void 0;`
     document.body.style.overflow = 'auto'
     a.scrollIntoView()
   }
